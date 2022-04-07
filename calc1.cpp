6fdbc8c546a7d946465f329aeef191459fb70dba
iMango77 <94566737+iMango77@users.noreply.github.com>
2022-04-07 11:36:27 -0400

    Add files via upload

---
+++ calc1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+int main()
+{
+
+  int w;
+  w = 3;
+  int l;
+  l = 4;
+  std::cout << "width?\n"
+  std::cin >> l
+
+  std::cout << "width " << w << '\n';
+  std::cout << "length " << l << '\n';
+  //std::cout << "Hello World!\n";
+  return 0;
+
+}
